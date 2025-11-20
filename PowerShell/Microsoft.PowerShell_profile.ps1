318e3d444a903eb5aac78ff9a9b7708a20f0cbd6
　kahvinkaunistama <58056752+kahvinkaunistama@users.noreply.github.com>
2025-11-20 17:32:10 +0200

    update: Added shell wrapper for yazi in powershell

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -26,6 +26,16 @@ Set-PSReadlineKeyHandler -Key 'Ctrl+r' -ScriptBlock {
     }
 }
 
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
+
 function o {
   cd "~/Obsidian-Vaults/myVault"
   nvim
